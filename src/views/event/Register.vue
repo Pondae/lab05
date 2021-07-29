62e8a9cfdc5611e52b1cf1a428bb2d69b234bf57
Pondae <phonmongkhon_ph@cmu.ac.th>
2021-07-29 17:00:47 +0700

    flash message with register button

--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -1,9 +1,26 @@
 <template>
   <p>Regstration form here</p>
+  <button @click="register">Register Me</button>
 </template>
 
 <script>
 export default {
-  props: ['event']
+  props: ['event'],
+  inject: ['GStore'],
+  methods: {
+    register() {
+      this.GStore.flashMessage =
+        'You are successfully registered for ' + this.event.title
+        setTimeout(() => {
+          // After 3 seconds remove it
+          this.GStore.flashMesage = ''
+        }, 3000)
+
+      this.$router.push({
+        name: 'EventDetails',
+        params: { id: this.event.id }
+      })
+    }
+  }
 }
 </script>
