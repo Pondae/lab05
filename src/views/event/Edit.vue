1b3d628fc6a34fae5665a4e1b318a54c1e37b376
Pondae <phonmongkhon_ph@cmu.ac.th>
2021-07-29 17:30:35 +0700

    (1.6)chk pt1: add edit button and show flashMessage inside Details page

--- src/views/event/Edit.vue
+++ src/views/event/Edit.vue
@@ -1,9 +1,26 @@
 <template>
   <p>Edit the event here</p>
+  <button @click="edit">Edit</button>
 </template>
 
 <script>
 export default {
-  props: ['event']
+  props: ['event'],
+  inject: ['GStore'],
+  methods: {
+    edit() {
+      this.GStore.flashMessageEdit =
+        'the data has been updated.'
+        setTimeout(() => {
+          // After 3 seconds remove it
+          this.GStore.flashMesageEdit = ''
+        }, 3000)
+
+      this.$router.push({
+        name: 'EventDetails',
+        params: { id: this.event.id }
+      })
+    }
+  }
 }
 </script>
